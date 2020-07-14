13d70ead37edc08062cb789684d732c1ce0d5369
Kyan Rowse <rowse9@gmail.com>
2020-07-14 21:47:17 +1000

    FMOD to gitignore & Implemented Music and Footsteps.

---
+++ Assets/Scripts/FMOD/F_Music.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+using FMOD.Studio;
+
+public class F_Music : MonoBehaviour
+{
+    public static EventInstance mainTheme;
+    void Start()
+    {
+        mainTheme = RuntimeManager.CreateInstance("event:/Music/Music (WIP)");
+        mainTheme.start();
+        mainTheme.release();
+    }
+}
---
+++ Assets/Scripts/FMOD/F_Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+using FMOD.Studio;
+
+public class F_Player : MonoBehaviour
+{
+    private CharacterController2D player;
+    EventInstance walk;
+
+    private void Start()
+    {
+        player = GetComponent<CharacterController2D>();
+        
+    }
+    public void Step()
+    {
+        if (player.m_Grounded == true)
+        {
+            walk = RuntimeManager.CreateInstance("event:/Player/Footsteps");
+            walk.start();
+            walk.release();
+        }     
+    }
+
+    void Jetpack()
+    {
+
+    }
+}
