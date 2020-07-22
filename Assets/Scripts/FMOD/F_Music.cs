53c0377b6a7c0867be7d0509f0c253cdfcc2f580
Kyan Rowse <rowse9@gmail.com>
2020-07-22 13:29:28 +1000

    Dynamic Music Implemented (WIP)

--- Assets/Scripts/FMOD/F_Music.cs
+++ Assets/Scripts/FMOD/F_Music.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 using FMOD;
 using FMODUnity;
 using FMOD.Studio;
+using System.Diagnostics.Contracts;
 
 public class F_Music : MonoBehaviour
 {
     public static EventInstance mainTheme;
+
+    public static bool musicStarted;
+
+    [SerializeField]
+    private float parameterValue;
     void Start()
     {
-        mainTheme = RuntimeManager.CreateInstance("event:/Music/Music (WIP)");
-        mainTheme.start();
-        mainTheme.release();
+        if (musicStarted == false)
+        {
+            mainTheme = RuntimeManager.CreateInstance("event:/Music/MusicDynamic");
+            mainTheme.start();
+            mainTheme.release();
+            musicStarted = true;
+        }   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mainTheme.setParameterByName("VerseChange", parameterValue, false);
+        }
+
     }
 }
