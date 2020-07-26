df451b1f934e4b9cca507d30f78c40f0519f955e
Karl Eyre <karl_eyre@hotmail.com>
2020-07-26 20:45:26 +1000

    1/2 of main menu play on start

---
+++ Assets/IntroStarter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroStarter : MonoBehaviour
+{
+    Animator ani;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ani.GetBool("Start Video") == true)
+        {
+            PlayVideo();
+            ani.SetBool("Start Video", false);
+        }
+    }
+
+    public void PlayVideo()
+    {
+        GameObject vid = GameObject.Find("Video Player");
+        var VideoPlayer = vid.AddComponent<UnityEngine.Video.VideoPlayer>();
+        VideoPlayer.Play();
+        print("Vid Started");
+    }
+}
