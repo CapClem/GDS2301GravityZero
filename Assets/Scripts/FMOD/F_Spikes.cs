4d2ed96c70986cd090cba78ed82eb7e6b1c2ba74
Kyan Rowse <rowse9@gmail.com>
2020-08-06 15:26:54 +1000

    Spikes & Boundary Death Sounds + Health

---
+++ Assets/Scripts/FMOD/F_Spikes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_Spikes : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            RuntimeManager.PlayOneShot("event:/Player/DeathRocks", default);
+        }
+    }
+}
+
