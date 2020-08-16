bbff5dbce3977d8d91dc0f0c0efd10631b5431bb
Kyan Rowse <rowse9@gmail.com>
2020-08-16 13:59:42 +1000

    TileFallSound / Optimized Audio

---
+++ Assets/Scripts/FMOD/F_FallingTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_FallingTile : MonoBehaviour
+{
+
+    private bool impact;
+
+    [FMODUnity.EventRef]
+    public string eventPath;
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player" && impact == false)
+        {
+
+            RuntimeManager.PlayOneShotAttached(eventPath, this.gameObject);
+            impact = true;
+        }
+    }
+}
