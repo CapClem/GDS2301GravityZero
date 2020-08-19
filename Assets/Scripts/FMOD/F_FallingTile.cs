5d7cb89add827f9a225b39e5a6cc2f2ac94df4d5
Kyan Rowse <rowse9@gmail.com>
2020-08-19 18:06:20 +1000

    Audio Balance

--- Assets/Scripts/FMOD/F_FallingTile.cs
+++ Assets/Scripts/FMOD/F_FallingTile.cs
@@ -9,15 +9,11 @@ public class F_FallingTile : MonoBehaviour
 
     private bool impact;
 
-    [FMODUnity.EventRef]
-    public string eventPath;
-
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player" && impact == false)
         {
-
-            RuntimeManager.PlayOneShotAttached(eventPath, this.gameObject);
+            RuntimeManager.PlayOneShotAttached("event:/Environment/FallingTile", this.gameObject);
             impact = true;
         }
     }
