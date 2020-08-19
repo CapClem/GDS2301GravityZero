9d134d327da2af96a434a7860f11346ca40e9eb2
Kyan Rowse <rowse9@gmail.com>
2020-08-19 16:59:57 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/FMOD/F_VolumeControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine.UI;
+
+public class F_VolumeControl : MonoBehaviour
+{
+    Bus bus;
+
+    private string busBath;
+    void Start()
+    {
+        bus = RuntimeManager.GetBus("bus/:" + busBath);
+    }
+
+    public void SliderVolume(float sliderValue)
+    {
+        bus.setVolume(sliderValue);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
