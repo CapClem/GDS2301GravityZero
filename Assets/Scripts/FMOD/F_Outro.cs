ab4498eea8cf1042db47f5848e94209bb4801f21
Kyan Rowse <rowse9@gmail.com>
2020-08-19 18:41:19 +1000

    Music Fade Out / Also turned off unity audio

--- Assets/Scripts/FMOD/F_Outro.cs
+++ Assets/Scripts/FMOD/F_Outro.cs
@@ -9,6 +9,7 @@ public class F_Outro : MonoBehaviour
     EventInstance outro;
     void Start()
     {
+        F_Music.mainTheme.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         outro = RuntimeManager.CreateInstance("event:/Music/OutroAnimatic");
         outro.start();
         outro.release();
