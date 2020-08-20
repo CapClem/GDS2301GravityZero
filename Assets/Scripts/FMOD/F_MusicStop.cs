612f5b6b7b61fa66fceaf84f52c668bcf54ebaa8
Kyan Rowse <rowse9@gmail.com>
2020-08-20 17:31:20 +1000

    Fixed Music Not stopping when going back to menu

---
+++ Assets/Scripts/FMOD/F_MusicStop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class F_MusicStop : MonoBehaviour
+{
+    public void StopMusic()
+    {
+        F_Music.mainTheme.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
