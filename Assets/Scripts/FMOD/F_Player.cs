f9bd6fd49126f727a76840a52ff0b7370f80584c
Kyan Rowse <rowse9@gmail.com>
2020-08-20 12:19:44 +1000

    Improved Footstep Sounds

--- Assets/Scripts/FMOD/F_Player.cs
+++ Assets/Scripts/FMOD/F_Player.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
-using UnityEngine.Experimental.XR;
 
 public class F_Player : MonoBehaviour
 {
