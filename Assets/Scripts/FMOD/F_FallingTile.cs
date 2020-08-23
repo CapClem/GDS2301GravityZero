93c382de41aa313f7e74e00c927caa92d5563b56
Kyan Rowse <rowse9@gmail.com>
2020-08-23 19:18:28 +1000

    Fixed Audio Issue

--- Assets/Scripts/FMOD/F_FallingTile.cs
+++ Assets/Scripts/FMOD/F_FallingTile.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using FMOD;
 
 public class F_FallingTile : MonoBehaviour
 {
 
     private bool impact;
 
+
+
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player" && impact == false)
         {
-            RuntimeManager.PlayOneShotAttached("event:/Environment/FallingTile", this.gameObject);
+            RuntimeManager.PlayOneShot("event:/Environment/FallingTile", default);
             impact = true;
+            StartCoroutine(SoundReset());
         }
     }
+
+    IEnumerator SoundReset()
+    {
+        yield return new WaitForSeconds(8);
+        impact = false;
+    }
+
 }
